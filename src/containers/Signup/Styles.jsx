3cc35a2a73bd07d7cd61cb220f04b486bb18921a
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-23 17:02:40 +0000

    sample signup

---
+++ src/containers/Signup/Styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  input {
+    padding: 0.5rem;
+    border: 1px solid grey;
+    border-radius: 3px;
+  }
+`;
