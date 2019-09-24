0f46a893b073dc08759a5b79b7498d50ba600897
Tyler Gilchrist <tyler668@gmail.com>
2019-09-24 12:07:49 -0400

    Nav bar added, styled

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./App.css";
 import { Signup } from "./containers/Signup";
-import Login from './components/Login';
+// import { Login } from './containers/Login';
+import Login from './containers/Login/Login';
 import { Navigation } from './containers/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <header className="App-header">
-        <h2>Log in</h2>
-        <Login />
+      <h2>Log in</h2>
+      <Login />
 
-        <h2>Sign up</h2>
-        <Signup />
-      </header>
+      <h2>Sign up</h2>
+      <Signup />
       <Router>
         <Switch>
           <Route exact path="/" />
