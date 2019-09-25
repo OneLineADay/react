a065d083778f9f095c4b3c80eb36ebe344f9fae2
Gatrrr <davidgatrmacaulay@gmail.com>
2019-09-25 12:53:19 -0700

    added some styling

--- src/containers/Entries/EntriesCard.js
+++ src/containers/Entries/EntriesCard.js
@@ -1,12 +1,30 @@
 import React from "react";
-import Styled from "styled-components";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+
+background-color: #EDFBFF;
+width: 600px; 
+max-height: 125px;
+margin: auto;
+border-bottom: 2px solid #00FFFF;
+border-right: 2px solid #00FFFF;
+`
+const StyledDiv2 = styled.div`
+background-color: #FFE9A4;
+max-width:50%;
+justify-content:flex-end;
+margin-left:45%;
+`
 
 const EntriesCard = props => {
     return (
-        <div className="">
-            <h2>Date</h2>
-            <h2>{props.text}</h2>
-        </div>
+        <StyledDiv2>
+            <StyledDiv>
+                <h2>Date</h2>
+                <h2>{props.text}</h2>
+            </StyledDiv>
+        </StyledDiv2>
     )
 }
 
