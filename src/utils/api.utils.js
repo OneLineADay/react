d51e3274a939756e8f4759d48fe0870236130dac
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-27 01:26:41 +0000

    states

--- src/utils/api.utils.js
+++ src/utils/api.utils.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import {store} from 'redux/store';
-// const token = store.getState().user.token;
 
 axios.defaults.baseURL = "https://olad-backend.herokuapp.com";
 axios.interceptors.request.use(config => {
